2d4d81a080010b123977630acb94d583dd9f0fe3
h-kirkwood <56115140+h-kirkwood@users.noreply.github.com>
2019-11-04 11:25:52 +0000

    Changes to random forests

--- random_forest.py
+++ random_forest.py
@@ -17,28 +17,39 @@ data = pd.read_csv("framingham.csv")
 X_train, X_test, y_train, y_test = split_data(scale_data(drop_missing(chose_features(data))))
 
 
-def scores(name, y_test, pred):
+def scores(name, y_test, pred, display):
     accuracy = accuracy_score(y_test, pred)
     f1 = f1_score(y_test, pred)
     precision = precision_score(y_test, pred)
     recall = recall_score(y_test, pred)
 
-    print("The accuracy score for {} is: {}%.".format(name, round(accuracy, 3) * 100))
-    print("The f1 score for {} is: {}%.".format(name, round(f1, 3) * 100))
-    print("The precision score for {} is: {}%.".format(name, round(precision, 3) * 100))
-    print("The recall score for {} is: {}%.".format(name, round(recall, 3) * 100))
+    if display == 'y':
+        print("The accuracy score for {} is: {}%.".format(name, round(accuracy, 3) * 100))
+        print("The f1 score for {} is: {}%.".format(name, round(f1, 3) * 100))
+        print("The precision score for {} is: {}%.".format(name, round(precision, 3) * 100))
+        print("The recall score for {} is: {}%.".format(name, round(recall, 3) * 100))
+        return(accuracy, f1, precision, recall)
+    elif display == 'n':
+        return(accuracy, f1, precision, recall)
+    else:
+        print("Error: please choose 'y' or 'n'.")
+
+
+def model(classifier, name, X_train, X_test, y_train, y_test, display):
+    classifier.fit(X_train, y_train)
+    classifier_pred = classifier.predict(X_test)
+    score = scores(name, y_test, classifier_pred, display)
+
+    accuracy = score[0]
+    f1 = score[1]
+    precision = score[2]
+    recall = score[3]
 
     return(accuracy, f1, precision, recall)
 
 
-def model(classifier, name, X_train, X_test, y_train, y_test):
-    classifier.fit(X_train, y_train)
-    classifier_pred = classifier.predict(X_test)
-    scores(name, y_test, classifier_pred)
-
-
 print("Decision Tree")
-model(DecisionTreeClassifier(), 'Decision Tree', X_train, X_test, y_train, y_test)
+model(DecisionTreeClassifier(), 'Decision Tree', X_train, X_test, y_train, y_test, 'y')
 
 print("Random Forest")
-model(RandomForestClassifier(n_estimators=10000, bootstrap=True, max_features='sqrt'), 'Random Forest', X_train, X_test, y_train, y_test)
+model(RandomForestClassifier(n_estimators=1000, bootstrap=True, max_features='sqrt'), 'Random Forest', X_train, X_test, y_train, y_test, 'y')
