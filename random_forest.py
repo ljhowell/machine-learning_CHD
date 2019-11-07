1268d60e5c5caf56898d59943ad75401bc72eec4
h-kirkwood <56115140+h-kirkwood@users.noreply.github.com>
2019-11-07 13:58:16 +0000

    Added verbose

--- random_forest.py
+++ random_forest.py
@@ -17,28 +17,28 @@ data = pd.read_csv("framingham.csv")
 X_train, X_test, y_train, y_test = split_data(scale_data(drop_missing(chose_features(data))))
 
 
-def scores(name, y_test, pred, display):
+def scores(name, y_test, pred, v):
     accuracy = accuracy_score(y_test, pred)
     f1 = f1_score(y_test, pred)
     precision = precision_score(y_test, pred)
     recall = recall_score(y_test, pred)
 
-    if display == 'y':
+    if v == 'y':
         print("The accuracy score for {} is: {}%.".format(name, round(accuracy, 3) * 100))
         print("The f1 score for {} is: {}%.".format(name, round(f1, 3) * 100))
         print("The precision score for {} is: {}%.".format(name, round(precision, 3) * 100))
         print("The recall score for {} is: {}%.".format(name, round(recall, 3) * 100))
         return(accuracy, f1, precision, recall)
-    elif display == 'n':
+    elif v == 'n':
         return(accuracy, f1, precision, recall)
     else:
         print("Error: please choose 'y' or 'n'.")
 
 
-def model(classifier, name, X_train, X_test, y_train, y_test, display):
+def model(classifier, name, X_train, X_test, y_train, y_test, v):
     classifier.fit(X_train, y_train)
     classifier_pred = classifier.predict(X_test)
-    score = scores(name, y_test, classifier_pred, display)
+    score = scores(name, y_test, classifier_pred, v)
 
     accuracy = score[0]
     f1 = score[1]
